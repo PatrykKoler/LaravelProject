4f8eb0ff77af6bda54fc6101e776f5081d891b09
PatrykKoler <syzyfinhoxd@gmail.com>
2022-11-08 19:29:28 +0100

    refactor route and create GradesController and ClassesController

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('register')
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dodanie nowego użytkownika</h1>
+            <h1 class="h2">Add new user</h1>
         </div>
         <form method="POST" action="{{ route('register') }}">
             @csrf
