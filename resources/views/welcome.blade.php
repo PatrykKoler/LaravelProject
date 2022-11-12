47626685e3b105bb19a2540fafe578e373c1b96a
PatrykKoler <syzyfinhoxd@gmail.com>
2022-11-12 16:23:33 +0100

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,18 @@
             <h4>Users</h4>
             <a href="/users"><img src="https://cdn-icons-png.flaticon.com/512/2146/2146937.png" class="main-img p-4"></a>
           </div>
+          <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+            <h4>Grades</h4>
+            <a href="/grades"><img src="https://cdn-icons-png.flaticon.com/512/2228/2228715.png" class="main-img p-4"></a>
+          </div>
+          <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+            <h4>Classes</h4>
+            <a href="/classes"><img src="https://cdn-icons-png.flaticon.com/512/943/943271.png" class="main-img p-4"></a>
+          </div>
+        </div>
+      </div>
+      @endif
+
       @if(Gate::check('isTeacher') || Gate::check('isStudent'))
       <div class="container text-center">
         <div class="row align-items-center">
@@ -27,6 +39,5 @@
           </div>
         </div>
       </div>
-      @endif
       @endif
 @endsection     
